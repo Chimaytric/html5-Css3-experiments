7011eebf8ad1ad1ec94a6f84e78e97f143f1b4ee
Charles MILLEQUANT <charles@cogitstudio.com>
2014-02-18 19:49:17 +0100

    bloc notes update

--- interface-bloc-notes/script.js
+++ interface-bloc-notes/script.js
@@ -1,10 +1,25 @@
 function addSheet(){
 	var liste = document.getElementById('liste');
 	var newElement = document.createElement('li');
-	var texte = document.createTextNode('Titre');
-	newElement.appendChild(texte);
+	
+	var titre = document.createElement('h1');
+	var titreTexte = document.createElement('input');
+	titreTexte.setAttribute('type', 'text');
+	titreTexte.setAttribute('placeholder', 'Nouvelle note');
+	
+	var deleteButton = document.createElement('span');
+	var deleteCross = document.createTextNode('x');
+	deleteButton.appendChild(deleteCross);
+	deleteButton.addEventListener("click", function(){
+		var toDelete = deleteButton.parentNode;
+		liste.removeChild(toDelete);
+	}, false);
+	
+	titre.appendChild(titreTexte);
+	newElement.appendChild(titre);
+	newElement.appendChild(deleteButton);
 	liste.appendChild(newElement);
 }
 
-var addButton = document.getElementById('addButton')
+var addButton = document.getElementById('addButton');
 addButton.addEventListener("click", addSheet, false);
